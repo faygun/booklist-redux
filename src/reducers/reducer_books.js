b673f29c5b8e68a704f1df7e4f05481b857de125
KARIYER\ahmet.aygun <faygun89@gmail.com>
2018-08-15 09:40:33 +0300

    build.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function(){
     return[
-        {title: 'Javascript : The Good Parts', pages:124},
+        {title: 'Javascript : The Good Parts', pages:125},
         {title: 'Harry Potter', pages:87},
         {title: 'Two Dark Tower', pages:45},
         {title: 'Eloquent Ruby', pages:58}
